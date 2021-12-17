03d61ac8b1e6cbe326070492b109f09ce1169806
Rohan Rajesh Kalbag <46604893+rohankalbag@users.noreply.github.com>
2021-12-17 10:09:29 +0530

    Added Stack

---
+++ stack.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+using namespace std;
+
+struct stack_node{
+    //same as node in SLL
+    int val;
+    stack_node * next;
+
+    stack_node(int x){
+        val = x;
+        next = NULL;
+    }
+};
+
+struct stack{
+    stack_node * top;
+    
+    stack(){
+        //constructor
+        top = NULL;
+    }
+
+    ~stack(){
+        //destructor
+        while(top){
+            pop();
+        }
+    }
+
+    void push(int x){
+        stack_node * temp = new stack_node(x);
+        temp->next = top;
+        top = temp;
+        cout<<x<<" was pushed"<<endl;
+    }
+
+    bool isEmpty(){
+        if(top==NULL){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+    void pop(){
+        if(isEmpty()){
+            cout<<"Stack Underflow !"<<endl;
+        }
+        else{
+            int popped_val = top->val;
+            stack_node* temp = top;
+            top = top->next;
+            delete(temp);
+            cout<<popped_val<<" was popped"<<endl;
+        }
+    }
+
+    void peek(){
+        if(isEmpty()){
+            cout<<"Stack Underflow !"<<endl;
+            return;
+        }
+        else{
+            cout<<"The topmost value is "<<top->val<<endl;
+        }
+    }
+
+    void print(){
+        stack_node* temp = top;
+        cout<<"top ->"<<" ";
+        while(temp){
+            cout<<temp->val<<"|";
+            temp = temp->next;
+        }
+        cout<<"]"<<endl;
+    }
+};
+
+int main(){
+    stack x;
+    x.push(1);
+    x.push(2);
+    x.push(3);
+    x.print();
+    x.peek();
+    x.pop();
+    x.peek();
+    x.pop();
+    x.peek();
+    x.pop();
+    x.peek();
+}
