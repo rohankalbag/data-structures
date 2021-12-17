49f06c237fc7fb8374fb129855bc2bba2044bbcc
Rohan Rajesh Kalbag <46604893+rohankalbag@users.noreply.github.com>
2021-12-17 09:13:13 +0530

    Added funcs

--- doublylinkedlist.cpp
+++ doublylinkedlist.cpp
@@ -6,6 +6,12 @@ struct node{
     int val;
     node * next;
     node * prev;
+
+    node(int x){
+        next=NULL;
+        prev=NULL;
+        val = x;
+    }
 };
 
 struct dllist{
@@ -29,8 +35,53 @@ public:
         }
     }
 
-    int length(){
-        
+    void print(){
+        //Complexity O(n)
+        node *n = head;
+        while(n!=NULL){
+            cout << "[" << n->val << "]" << "=";
+            n = n->next;
+        }
+        cout<<">"<<endl;
     }
 
+    int length(){
+        return size;
+    }
+
+    void insert_at_head(int x){
+        node* n = new node(x);
+        n->next = head;
+        head->prev = n;
+        head = n;
+        size++;
+    }
+
+    void insert_at_tail(int x){
+        node* n = new node(x);
+        tail->next = n;
+        n->prev = tail;
+        tail = n;
+        size++;
+    }
+    
+    node* get_head(){
+        return head;
+    }
+
+    node* get_tail(){
+        return tail;
+    }
 };
+
+int main(){
+    dllist dll;
+    dll.insert_at_head(1);
+    dll.print();
+    dll.insert_at_head(3);
+    dll.print();
+    dll.insert_at_head(4);
+    dll.print();
+    dll.insert_at_tail(2);
+    cout<<dll.length()<<endl;
+}
