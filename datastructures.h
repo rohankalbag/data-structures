cc7084b8105c9fc4b29f32dfd261cec25f9b504b
Rohan Rajesh Kalbag <46604893+rohankalbag@users.noreply.github.com>
2021-12-22 22:50:16 +0530

    added BST

--- datastructures.h
+++ datastructures.h
@@ -483,3 +483,67 @@ struct deque{
     }
 
 };
+
+//BST
+
+struct bt_node{
+    int val;
+    bt_node* right;
+    bt_node* left;
+    
+    bt_node(int x){
+        right=NULL;
+        left= NULL;
+        val = x;
+    }
+};
+
+struct bs_tree{
+    bt_node* root;
+    
+    bs_tree(){
+        root = NULL;
+    }
+
+    void insert(bt_node* &node, int val){
+        if(node==NULL){
+            node = new bt_node(val);
+        }
+        else{
+            if(val==node->val){
+                return;
+            }
+            else if(val>node->val){
+                insert(node->right,val);
+            }
+            else{
+                insert(node->left,val);
+            }
+        }
+    }
+
+    bool find(bt_node* &node, int val){
+        if(node==NULL){
+            return false;
+        }
+        if(val==node->val){
+                return true;
+        }
+        else{
+            if(val>node->val){
+                return find(node->right,val);
+            }
+            else{
+                return find(node->left, val);
+            }
+        }
+    }
+
+    void insert(int val){
+        insert(root,val);
+    }
+
+    bool find(int k){
+        return find(root,k);
+    }
+};
