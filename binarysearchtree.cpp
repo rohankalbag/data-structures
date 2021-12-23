214821e0e9991ee960d724b7ae69063da613895f
Rohan Rajesh Kalbag <46604893+rohankalbag@users.noreply.github.com>
2021-12-23 18:30:55 +0530

    added traversal

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -15,7 +15,8 @@ struct bt_node{
 
 struct bs_tree{
     bt_node* root;
-    
+    int size;
+
     bs_tree(){
         root = NULL;
     }
@@ -23,6 +24,7 @@ struct bs_tree{
     void insert(bt_node* &node, int val){
         if(node==NULL){
             node = new bt_node(val);
+            size++;
         }
         else{
             if(val==node->val){
@@ -61,6 +63,48 @@ struct bs_tree{
     bool find(int k){
         return find(root,k);
     }
+
+    void inorder(bt_node* &node){
+        if(node==NULL){
+            return;
+        }
+        inorder(node->left);
+        cout<<node->val<<" ";
+        inorder(node->right);
+    }
+
+    void preorder(bt_node* &node){
+        if(node==NULL){
+            return;
+        }
+        cout<<node->val<<" ";
+        preorder(node->left);
+        preorder(node->right); 
+    }
+
+    void postorder(bt_node* &node){
+        if(node==NULL){
+            return;
+        }
+        postorder(node->left);
+        postorder(node->right);
+        cout<<node->val<<" ";
+    }
+
+    void inorder(){
+        inorder(root);
+        cout<<endl;
+    }
+
+    void preorder(){
+        preorder(root);
+        cout<<endl;
+    }
+
+    void postorder(){
+        postorder(root);
+        cout<<endl;
+    }
 };
 
 
@@ -76,10 +120,35 @@ int main(){
         cin>>x;
         tree.insert(x);
     }
-    
+    bool stop = false;
+
+    while(!stop){
+        cout<<"Enter 1 to traverse the tree inorder"<<endl;
+        cout<<"Enter 2 to traverse the tree preorder"<<endl;
+        cout<<"Enter 3 to traverse the tree postorder"<<endl;
+        cout<<"Enter 0 to continue to next part"<<endl;
+        int x;
+        cin>>x;
+        switch(x){
+            case 0:
+                stop = true;
+                break;
+            case 1:
+                tree.inorder();
+                break;
+            case 2:
+                tree.preorder();
+                break;
+            case 3:
+                tree.postorder();
+                break;
+            default:
+                cout<<"Invalid entry"<<endl;
+                break;
+        }
+    }
     int k;
     cout<<"Enter value to find"<<endl;
     cin>>k;
     tree.find(k) ? cout<<"Found"<<endl : cout<<"Not Found"<<endl;
-
 }
